3ffd940eed595b2072a0339908d2fe02f669099f
Vinny Yip <yipvinny@yahoo.com>
2024-11-09 03:34:43 -0800

    Added pseudocode for GET Request Endpoint .. waiting on consensus to go ahead with implementation

--- backend/routes.py
+++ backend/routes.py
@@ -18,3 +18,15 @@ def get_data():
 def upload_file_route():  # Defines a route function that calls the upload_file function
     time.sleep(20)
     return upload_file()  # Calls the uploaded file function that was imported
+
+#Functionality GET Request
+#@upload_blueprint.route('/process_audio', methods=['GET'])
+    #Pseudocode
+    # Use query parameter?? to pass audio file
+
+    # Retrieve file .. download_audio
+
+    #Trim audio using function trim_audio function I implemented in input_upload.py
+    #Then analyze the audio
+
+    #Return analysis result
