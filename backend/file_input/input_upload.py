47b5517914607436c6cf9ab8494c98b3b3b071e8
Vinny Yip <yipvinny@yahoo.com>
2024-11-09 02:50:09 -0800

    Removed redundant code for loading/analyzing audio files

--- backend/file_input/input_upload.py
+++ backend/file_input/input_upload.py
@@ -15,7 +15,7 @@ def upload_file():
         return jsonify({"error": "No file part"}), 400
 
     file = request.files['file']
-    
+
     # If no file is selected, can probably handle this in frontend?
     if file.filename == '':
         return jsonify({"error": "No selected file"}), 400
@@ -23,8 +23,8 @@ def upload_file():
     # Checks the file extension; fix later
     allowed_extensions = {'.mp3', '.wav'}
     file_extension = file.filename.rsplit('.', 1)[-1].lower()
-    
-    
+
+
     # Attempt to load audio using librosa
     try:
 
@@ -32,21 +32,17 @@ def upload_file():
         audio_data = file.read()  # Read the file content
         # Load the audio from the BytesIO stream
         y, sr = librosa.load(io.BytesIO(audio_data), sr=None)
-        
+
         # Check getting bypassed on an all zero array
         if np.all(y == 0):
             print("The array contains only zeros.")
             return jsonify({"error": "Audio file could not be processed or it is silent."}), 400
 
+        result = analyze_audio(audio_data)
+
     except Exception as e:
         return jsonify({"error": f"File could not be processed. Error: {str(e)}"}), 400
 
-
-    # Reset file pointer before passing to analyze audio function
-    file.seek(0)
-    filetmp = file.read()
-    result = analyze_audio(filetmp)
-    
     # return the analysis result
     return jsonify(result), 200
 
