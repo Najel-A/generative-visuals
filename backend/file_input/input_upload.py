eaae78f74e9824914034a5942c2a1c7b5e8e305a
Najel-A <59577073+Najel-A@users.noreply.github.com>
2024-11-08 22:16:30 -0800

    trouble with returning output on upload_file

--- backend/file_input/input_upload.py
+++ backend/file_input/input_upload.py
@@ -1,4 +1,9 @@
 from flask import Flask, request, jsonify
+import librosa
+import soundfile as sf
+import io
+from .generate import *
+
 '''
 TODO: Handle file input here
 '''
@@ -9,25 +14,43 @@ def upload_file():
         return jsonify({"error": "No file part"}), 400
 
     file = request.files['file']
-
-    # If no file is selected
+    
+    # If no file is selected, can probably handle this in frontend
     if file.filename == '':
         return jsonify({"error": "No selected file"}), 400
 
     # Checks the file extension
     allowed_extensions = {'.mp3', '.wav'}
     file_extension = file.filename.rsplit('.', 1)[-1].lower()
+    
+    # Inspect File for librosa
+    # Reset file pointer to the beginning of the file
+    file.seek(0)
+    
+    # Attempt to load audio using librosa
+    try:
+        audio_data = file.read()  # Read the file content
+        # Load the audio from the BytesIO stream
+        y, sr = librosa.load(io.BytesIO(audio_data), sr=None)
+        print(f"Normalized audio data: {y}")
+        print(f"Shape of y: {y.shape}, Sample Rate: {sr}")
+        print(y, sr)
+        
+        if len(y) == 0:
+            return jsonify({"error": "File could not be processed. Audio is empty."}), 400
 
-    if f".{file_extension}" not in allowed_extensions:
-        return jsonify({"error": "Unsupported file type. Only .mp3 and .wav are allowed."}), 400
+        #return jsonify({"message": "File uploaded successfully", "sample_rate": sr, "duration": len(y) / sr}), 200
 
-    # Temporary Return
-    '''
-    @TODO:
-        Return frames and sequence?
-    '''
-    result = "OK"
-    return jsonify({"message": "File uploaded successfully", "result": result}), 200
+    except Exception as e:
+        return jsonify({"error": f"File could not be processed. Error: {str(e)}"}), 400
+
+
+    file.seek(0)
+    filetmp = file.read()
+    result = analyze_audio(filetmp)
+
+    # return the analysis result
+    return jsonify(result), 200
 
 
 def test():
