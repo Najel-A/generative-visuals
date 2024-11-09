fd79329c0e115c4935ec9e1f4367c202b033ff24
Vinny Yip <yipvinny@yahoo.com>
2024-11-09 03:03:25 -0800

    Added 20 second timer to simulate delay

--- backend/routes.py
+++ backend/routes.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, request, jsonify
 from file_input.input_upload import *
+import time
 
 '''
-    To reduce space in the routes, going to create functions in separate directories and just import it here
+     To reduce space in the routes, going to create functions in separate directories and just import it here
 '''
 
 upload_blueprint = Blueprint('upload', __name__) # Creates a blueprint for the app.py
@@ -15,4 +16,5 @@ def get_data():
 # File Upload Input
 @upload_blueprint.route('/upload', methods=['POST'])
 def upload_file_route():  # Defines a route function that calls the upload_file function
+    time.sleep(20)
     return upload_file()  # Calls the uploaded file function that was imported
