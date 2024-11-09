754c5d57c37ddb38510ef771aab7a3c5e5dca198
Najel-A <59577073+Najel-A@users.noreply.github.com>
2024-11-08 23:53:19 -0800

    Connecting for EC2

--- backend/app.py
+++ backend/app.py
@@ -12,5 +12,6 @@ CORS(app)  # Enables CORS for all routes; Allows for running on different ports
 app.register_blueprint(upload_blueprint) # Register blueprint
 
 
+# Change for 0.0.0.0 for EC2
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
